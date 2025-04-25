9dabe23fede99bda38113f63d31fd5bb661ef4d4
Pro7Prabhakar <prabhashankarmishra202@gmail.com>
2025-04-25 21:31:11 +0530

    Prepare Django for Render deployment

--- notes_project/notes_project/settings.py
+++ notes_project/notes_project/settings.py
@@ -126,3 +126,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import os
+
+ALLOWED_HOSTS = ['*'] # Or ['your-service-name.onrender.com'] for security
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
